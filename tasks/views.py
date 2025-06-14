7c270a95af2ae45df3e14c0b2c71940edd3a3363
Jenemina <122135250+Neminita@users.noreply.github.com>
2025-06-13 19:13:37 -0600

    Base proyecto

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
 
-# Create your views here.
+def signup(request): #FORMULARIO CREACIÓN DE CUENTA
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+        'form' :UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form' :UserCreationForm,
+                    'error':'Usuario ya existe'
+                })
+            
+        return render(request, 'signup.html', {
+                    'form' :UserCreationForm,
+                    'error':'Contraseñas no coinciden'
+                })
+
+
+def home(request):
+    return render(request, 'home.html',)
+
+def base(request):
+    return render(request, 'base.html',)
+
+
