d0f0242cd05bbca891eac5329fa7c7dd9e3d5d48
Jenemina <122135250+Neminita@users.noreply.github.com>
2025-06-14 21:45:28 -0600

    Registro, sesión usuarios, base de datos

--- ferremax/urls.py
+++ ferremax/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
-    path('base/', views.base, name='base')
+    path('base/', views.base, name='base'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 def signup(request): #FORMULARIO CREACIÓN DE CUENTA
@@ -35,3 +35,26 @@ def home(request):
 
 def base(request):
     return render(request, 'base.html',)
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html',{
+        'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Usuario o contraseña erróneos'
+            })
+        else:
+            login(request, user)
+            return redirect('home')
+
